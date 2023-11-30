69809cbe742278e79b002323784f4e3079863b8f
Sathish <sathishbabur@hotmail.com>
2023-11-30 20:05:24 +0530

    navigation added

--- src/app/user-components/home/home.component.ts
+++ src/app/user-components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../../general/footer/footer.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FooterComponent],
+  imports: [CommonModule, FooterComponent, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
