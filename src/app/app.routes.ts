1f303730b1aa12be02ba0e128ff229ca621669cb
Sathish <sathishbabur@hotmail.com>
2023-12-01 08:25:26 +0530

    login and forgot empty component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './user-components/home/home.component';
 import { DashboardComponent } from './user-components/dashboard/dashboard.component';
+import { LoginComponent } from './guest-components/login/login.component';
+import { ForgotPasswordComponent } from './guest-components/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
-
     {
-        path: '', component: HomeComponent,
+        path: '',
+        component: LoginComponent
+    },
+    {
+        path: 'forgotPassword',
+        component: ForgotPasswordComponent
+    },
+    {
+        path: 'home', component: HomeComponent,
         children: [{
             path: '', component: DashboardComponent
         }]
